2e3153a0b72ba26723f9637fabe0105a6eaca5ef
cooperbraun13 <cooperjbraun13@gmail.com>
2025-02-21 15:06:12 -0800

    got api working and working directory

--- main.py
+++ main.py
@@ -5,25 +5,30 @@ import matplotlib.pyplot as plt
 from fredapi import Fred
 
 API_KEY = "92b9d033043b6c4d48afcb2115d97839"
+
+# initialize FRED API client
 fred = Fred(api_key=API_KEY)
 
 # fetch economic data
 gdp_growth = fred.get_series("A191RL1Q225SBEA")
+# computes percent change and then multiplies by 100 to convert to a percentage
 inflation = fred.get_series("CPIAUCSL").pct_change() * 100
 unemployment = fred.get_series("UNRATE")
 
+# convert to datetime format
 df = pd.DataFrame({
     "GDP Growth (%)": gdp_growth,
     "Inflation Rate (%)": inflation,
     "Unemployment Rate (%)": unemployment
 })
 
+# any rows with missing values are removed
 df.index = pd.to_datetime(df.index)
 df.dropna(inplace=True)
 
-# Define your directory and file path
-data_dir = "data"  # adjust if needed
-os.makedirs(data_dir, exist_ok=True)  # Create the directory if it doesn't exist
+data_dir = "data"
+# created the directory because it wasn't recognizing my folder
+os.makedirs(data_dir, exist_ok=True)
 data_path = os.path.join(data_dir, "economic_data.csv")
 
 df.to_csv(data_path)
